82a03197f2b70ee6a98c817b7beafeb0b210ebd4
Rocca-Francesco <roccafrance98@yahoo.it>
2023-01-23 14:26:58 +0100

    calcolo sconto

--- js/myscript.js
+++ js/myscript.js
@@ -6,6 +6,24 @@
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
 // Chiedo all'utente quanti chilometri deve percorrere.
-let distanza = parseInt(prompt("Quanti chilometri devi percorrere?"))
+let distanza = parseInt(prompt("Quanti chilometri devi percorrere?"));
 
-console.log(distanza);
+// Chiedo l'età dell'utente.
+let userAge = parseInt(prompt("Quanti anni hai?"));
+
+// Calcolo il prezzo del biglietto.
+let ticketPrice = distanza * 0.21;
+
+// Calcolo lo sconto.
+
+if (userAge < 18) {
+    let discount = ((ticketPrice / 100) * 20);
+    ticketPrice = ticketPrice - discount;
+}
+else if (userAge >= 65) {
+    let discount = ((ticketPrice / 100) * 40);
+    ticketPrice = ticketPrice - discount;
+}
+
+
+console.log(distanza, userAge, ticketPrice);
