4a5086ae023926f96a9e1ed9b249434370c697b0
Rocca-Francesco <roccafrance98@yahoo.it>
2023-01-23 14:07:32 +0100

    ins distanza

---
+++ js/myscript.js
@@ -0,0 +1,11 @@
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// Il prezzo del biglietto è definito in base ai km (0.21 € al km).
+// Va applicato uno sconto del 20% per i minorenni.
+// Va applicato uno sconto del 40% per gli over 65.
+// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+// Chiedo all'utente quanti chilometri deve percorrere.
+let distanza = parseInt(prompt("Quanti chilometri devi percorrere?"))
+
+console.log(distanza);
